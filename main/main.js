b664bee0cbb6ee78e1ef3028e2974c0c8e18e779
Karpenko Elena <lena6331531@gmail.com>
2022-02-16 17:28:45 +0200

    Mistake correction

--- main/main.js
+++ main/main.js
@@ -1,5 +1,5 @@
 const p = 3.1415926;
-let data = prompt ('Введите радиус, будет рассчитана площать круга!');
+let data = prompt ('Введите радиус, будет рассчитана площадь круга!');
 let dataInsquare = data*data;
 let result = p*dataInsquare;
 alert('Площадь круга составляет: ' + result);
