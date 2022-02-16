8b4a5663a2a7407cf901e6e2b9b2f01d7b25c315
Karpenko Elena <lena6331531@gmail.com>
2022-02-16 17:25:14 +0200

    Home Work JS1 first version

---
+++ main/main.js
@@ -0,0 +1,5 @@
+const p = 3.1415926;
+let data = prompt ('Введите радиус, будет рассчитана площать круга!');
+let dataInsquare = data*data;
+let result = p*dataInsquare;
+alert('Площадь круга составляет: ' + result);
